addc0f461dcd703f342460fa4f0c6a3ffc4eaf44
noor16mustafa <c173212@ugrad.iiuc.ac.bd>
2022-11-02 22:00:33 +0600

    checkout added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
         </>
 
     return (
-        <div className="navbar h-12 pt-12 bg-base-100">
+        <div className="navbar h-12 pt-12 bg-base-100 mb-12">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
+import Checkout from "../../Pages/Checkout/Checkout";
 import Home from "../../Pages/Home/Home";
 import Login from "../../Pages/Login/Login";
 import SignUp from "../../SignUp/SignUp";
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
             {
                 path: '/signup',
                 element: <SignUp></SignUp>
+            }, {
+                path: '/checkout/:id',
+                element: <Checkout></Checkout>,
+                loader: ({ params }) => fetch(`http://localhost:5000/services/${params.id}`)
             }
         ]
     }
